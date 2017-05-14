44babe1b52b8bd439207c8c6cfa61309b0ce776a
Marc Abramowitz <abramowi@adobe.com>
2017-05-14 09:06:56 -0700

    Switch from go-etcd to etcd/client
    because the former is deprecated and hasn't been updated in years.

--- kvwrapper_etcd/kvwrapper_etcd.go
+++ kvwrapper_etcd/kvwrapper_etcd.go
@@ -1,29 +1,40 @@
 package kvwrapper_etcd
 
 import (
+	"context"
 	"strings"
+	"time"
 
 	"github.com/behance/go-common/kvwrapper"
-	"github.com/coreos/go-etcd/etcd"
+	etcd "github.com/coreos/etcd/client"
 )
 
 // EtcdWrapper wraps the go-etcd client so it can implement the KVWrapper interface
 type EtcdWrapper struct {
-	client *etcd.Client
+	kapi etcd.KeysAPI
 }
 
 // NewKVWrapper returns a new kvwrapper_etcd as a KVWrapper
 func (e EtcdWrapper) NewKVWrapper(servers []string, username, password string) kvwrapper.KVWrapper {
-	e.client = etcd.NewClient(servers)
-	if username != "" && password != "" {
-		e.client.SetCredentials(username, password)
+	config := etcd.Config{
+		Endpoints: servers,
+		Transport: etcd.DefaultTransport,
+		Username:  username,
+		Password:  password,
 	}
-	return e
+	client, err := etcd.New(config)
+	if err != nil {
+		panic(err)
+	}
+	return EtcdWrapper{kapi: etcd.NewKeysAPI(client)}
 }
 
-//Set stes the key = val with a ttl of ttl. If key is a path, it will be created.
+// Set sets the key = val with a ttl of ttl. If key is a path, it will be created.
 func (e EtcdWrapper) Set(key string, val string, ttl uint64) error {
-	_, err := e.client.Set(key, val, ttl)
+	options := &etcd.SetOptions{
+		TTL: time.Duration(ttl) * time.Second,
+	}
+	_, err := e.kapi.Set(context.Background(), key, val, options)
 	if err != nil {
 		return err
 	}
@@ -32,7 +43,11 @@ func (e EtcdWrapper) Set(key string, val string, ttl uint64) error {
 
 // GetVal returns a single KeyValue found at key
 func (e EtcdWrapper) GetVal(key string) (*kvwrapper.KeyValue, error) {
-	r, err := e.client.Get(key, false, true)
+	options := &etcd.GetOptions{
+		Sort:      false,
+		Recursive: true,
+	}
+	r, err := e.kapi.Get(context.Background(), key, options)
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "501:") {
 			return nil, kvwrapper.ErrCouldNotConnect
@@ -52,7 +67,11 @@ func (e EtcdWrapper) GetVal(key string) (*kvwrapper.KeyValue, error) {
 
 // GetVal returns a []KeyValue found at key
 func (e EtcdWrapper) GetList(key string, sort bool) ([]*kvwrapper.KeyValue, error) {
-	r, err := e.client.Get(key, sort, true)
+	options := &etcd.GetOptions{
+		Sort:      sort,
+		Recursive: true,
+	}
+	r, err := e.kapi.Get(context.Background(), key, options)
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "501:") {
 			return nil, kvwrapper.ErrCouldNotConnect
